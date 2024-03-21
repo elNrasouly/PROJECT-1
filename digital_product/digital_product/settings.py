796f2847b798a3ad320c73d463526e80fee0d6d4
elNrasouly <elyas1019rasuli@gmail.com>
2024-03-21 09:49:09 +0330

    products models added.

--- digital_product/digital_product/settings.py
+++ digital_product/digital_product/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'products',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'digital_product.urls'
@@ -116,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media Files
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- digital_product/products/models.py
+++ digital_product/products/models.py
@@ -1,16 +1,44 @@
 from django.db import models
 
-from django.utils.translation import ugettext_lazy as _
+#from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
-    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null = True, on_delete=models.CASCADE)
-    title = models.CharField(_('title'), max_length = 50)
-    description = models.TextField(_('description'), blank=True)
-    enable = models.BooleanField(_('enable'), default = True)
-    created_time = models.DateTimeField(_('created time'), auto_now_add = True)
-    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+    parent = models.ForeignKey('self', verbose_name='parent', blank=True, null = True, on_delete=models.CASCADE)
+    title = models.CharField('title', max_length = 50)
+    description = models.TextField('description', blank=True)
+    avatar = models.ImageField('avatar', upload_to='categories/')
+    enable = models.BooleanField('enable', default = True)
+    created_time = models.DateTimeField('created time', auto_now_add = True)
+    updated_time = models.DateTimeField('updated time', auto_now=True)
 
     class Meta:
         db_table = 'categories'
-        verbose_name = _('Categort')
-        verbose_name_plural = _('categories')
+        verbose_name = 'Category'
+        verbose_name_plural = 'categories'
+
+class Product(models.Model):
+    title = models.CharField('title', max_length = 50)
+    description = models.TextField('description', blank=True)
+    avatar = models.ImageField('avatar', upload_to='products/')
+    categories = models.ManyToManyField(Category, verbose_name='categories', blank = True)
+    enable = models.BooleanField('enable', default = True)
+    created_time = models.DateTimeField('created time', auto_now_add = True)
+    updated_time = models.DateTimeField('updated time', auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'products'
+
+class File(models.Model):
+    product = models.ForeignKey('product', verbose_name='product', on_delete=models.CASCADE)
+    title = models.CharField('title', max_length=50)
+    file = models.FileField('file', upload_to='files/%Y/%m/%d/')
+    enable = models.BooleanField('enable', default = True)
+    created_time = models.DateTimeField('created time', auto_now_add = True)
+    updated_time = models.DateTimeField('updated time', auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = 'File'
+        verbose_name_plural = 'files'
